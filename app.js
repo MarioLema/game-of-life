2b8dfa02f94408fd2fc2dd8f56159d4df7b11880
Mario Lema <lema_mario@yahoo.com>
2019-04-02 22:17:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//===========================================DATA OBJECT========================================================
+const DATA = {
+
+};
+
+//===========================================VIEW METHODS========================================================
+const VIEW = {
+};
+
+//===========================================MODIFIER METHODS========================================================
+const MODIFIER = {
+
+    //================PLAY GAME METHODS==================
+    //===================================================
+
+
+    //================VALIDATION METHODS==================
+    //===================================================
+
+
+    //================UTILITY METHODS==================
+    //===================================================
+
+ 
+
+    //================LISTENER METHODS==================
+    //===================================================
+
+
+}
+// MODIFIER.turn = MODIFIER.turn.bind(MODIFIER);
+// MODIFIER.startGame = MODIFIER.startGame.bind(MODIFIER);
+
+
+
+//========================CLICK EVENTS============================================
+// document.getElementById(`memory-game`).addEventListener(`click`, MODIFIER.turn, false);
+// document.getElementById(`start-game`).addEventListener(`click`, MODIFIER.startGame, false);
+// document.getElementById(`reset-game`).addEventListener(`click`, MODIFIER.resetGame, false);
+// MODIFIER.winkUpListener(document.querySelectorAll(`.card-front`));
