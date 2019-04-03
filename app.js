ce3395d615f74e5939a9c5d1a74f7beda2fa1316
Mario Lema <lema_mario@yahoo.com>
2019-04-03 18:58:19 +0100

    add speed and size options

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const DATA = {
     canvas: document.querySelector('canvas'),
     resolution: 10,
-    width: 900,
-    height: 900,
+    width: 1000,
+    height: 1000,
     cols() {
         return this.width / this.resolution
     },
@@ -12,6 +12,12 @@ const DATA = {
     },
     baseArr: [],
     nextArr: [],
+    playing: false,
+    intervalTime: 200,
+    timeout: null,
+    population: `average`,
+    canvasStyle: `emojimap`,
+
 
 
 };
@@ -26,19 +32,40 @@ const VIEW = {
         MODIFIER.render(DATA.baseArr);
     },
 
+    changeAlert(){
+        document.getElementById(`change-alert`).innerText = `CAN'T CHANGE OPTIONS WHILE PLAYING`;
+    }
+
 };
 
 //===========================================MODIFIER METHODS========================================================
 const MODIFIER = {
 
     startGame() {
-        requestAnimationFrame(this.update);
+        if (!DATA.playing) {
+            DATA.playing = true;
+            requestAnimationFrame(this.update);
+        }
+
+    },
+
+    resetGame() {
+        DATA.playing = false;
+        VIEW.generateCanvas();
+        clearTimeout(DATA.timeout);
+    },
+
+    pauseGame() {
+        if (DATA.playing) {
+            DATA.playing = false;
+            clearTimeout(DATA.timeout);
+        }
     },
 
     buildGrid() {
         return Array(DATA.cols()).fill(null)
-        .map(() => Array(DATA.rows()).fill(null)
-            .map(() => Math.floor(Math.random() * 2)));
+            .map(() => Array(DATA.rows()).fill(null)
+                .map(() => Math.floor(Math.random() * 2)));
     },
 
 
@@ -46,27 +73,30 @@ const MODIFIER = {
     update() {
         this.nextGen();
         this.render(DATA.nextArr);
-        requestAnimationFrame(this.update);
+        DATA.timeout = setTimeout(() => {
+            requestAnimationFrame(this.update);
+        }, DATA.intervalTime);
+
     },
 
-     render(grid) {
-    for (let col = 0; col < grid.length; col++) {
-        for (let row = 0; row < grid[col].length; row++) {
-            const cell = grid[col][row];
-            let ctx = DATA.canvas.getContext(`2d`);
+    render(grid) {
+        for (let col = 0; col < grid.length; col++) {
+            for (let row = 0; row < grid[col].length; row++) {
+                const cell = grid[col][row];
+                let ctx = DATA.canvas.getContext(`2d`);
 
-            ctx.beginPath();
-            ctx.rect(col * DATA.resolution, row * DATA.resolution, DATA.resolution, DATA.resolution);
-            ctx.fillStyle = cell ? 'black' : 'white';
-            ctx.fill();
-            // ctx.stroke();
+                ctx.beginPath();
+                ctx.rect(col * DATA.resolution, row * DATA.resolution, DATA.resolution, DATA.resolution);
+                ctx.fillStyle = cell ? 'black' : 'white';
+                ctx.fill();
+                // ctx.stroke();
+            }
         }
-    }
-},
+    },
 
     nextGen() {
         DATA.nextArr = DATA.baseArr.map(arr => [...arr]);
-    
+
         for (let col = 0; col < DATA.baseArr.length; col++) {
             for (let row = 0; row < DATA.baseArr[col].length; row++) {
                 const cell = DATA.baseArr[col][row];
@@ -78,14 +108,14 @@ const MODIFIER = {
                         }
                         const x_cell = col + i;
                         const y_cell = row + j;
-    
+
                         if (x_cell >= 0 && y_cell >= 0 && x_cell < DATA.cols() && y_cell < DATA.rows()) {
                             const currentNeighbour = DATA.baseArr[col + i][row + j];
                             numNeighbours += currentNeighbour;
                         }
                     }
                 }
-    
+
                 // rules
                 if (cell === 1 && numNeighbours < 2) {
                     DATA.nextArr[col][row] = 0;
@@ -99,7 +129,70 @@ const MODIFIER = {
 
         DATA.baseArr = DATA.nextArr.map(arr => [...arr]);
 
+    },
+
+    optionsChanger() {
+        if(!DATA.playing){
+        let id = event.target.id;
+        switch (id) {
+            case `slow`:
+                DATA.intervalTime = 500;
+                break;
+            case `regular`:
+                DATA.intervalTime = 200;
+                break;
+            case `fast`:
+                DATA.intervalTime = 0;
+                break;
+            case `black-white`:
+                DATA.canvasStyle = `black-white`;
+                VIEW.generateCanvas();
+                break;
+            case `heatmap`:
+                DATA.canvasStyle = `heatmap`;
+                VIEW.generateCanvas();
+                break;
+            case `emojimap`:
+                DATA.canvasStyle = `emojimap`;
+                VIEW.generateCanvas();
+                break;
+            case `small`:
+                DATA.width = 100;
+                DATA.height = 100;
+                DATA.resolution = 5;
+                VIEW.generateCanvas();
+                break;
+            case `medium`:
+                DATA.width = 500;
+                DATA.height = 500;
+                DATA.resolution = 5;
+                VIEW.generateCanvas();
+                break;
+            case `large`:
+                DATA.width = 1000;
+                DATA.height = 1000;
+                DATA.resolution = 10;
+                VIEW.generateCanvas();
+                break;
+            case `low`:
+                DATA.population = `low`;
+                VIEW.generateCanvas();
+                break;
+            case `average`:
+                DATA.population = `average`;
+                VIEW.generateCanvas();
+                break;
+            case `high`:
+                DATA.population = `high`;
+                VIEW.generateCanvas();
+                break;
+            default:
+                return
+        }
+    }else{
+        VIEW.changeAlert();
     }
+    },
     //================PLAY GAME METHODS==================
     //===================================================
 
@@ -115,96 +208,23 @@ const MODIFIER = {
 
     //================LISTENER METHODS==================
     //===================================================
-
+    optionsListener() {
+        let radioButtons = document.querySelectorAll(`input`);
+        radioButtons.forEach(x => x.addEventListener(`click`, MODIFIER.optionsChanger));
+    },
 
 }
-// MODIFIER.turn = MODIFIER.turn.bind(MODIFIER);
 MODIFIER.startGame = MODIFIER.startGame.bind(MODIFIER);
 MODIFIER.nextGen = MODIFIER.nextGen.bind(MODIFIER);
 MODIFIER.update = MODIFIER.update.bind(MODIFIER);
 
 
 //========================CLICK EVENTS============================================
-// document.getElementById(`memory-game`).addEventListener(`click`, MODIFIER.turn, false);
 document.getElementById(`start-game`).addEventListener(`click`, MODIFIER.startGame, false);
-// document.getElementById(`reset-game`).addEventListener(`click`, MODIFIER.resetGame, false);
-// MODIFIER.winkUpListener(document.querySelectorAll(`.card-front`));
+document.getElementById(`reset-game`).addEventListener(`click`, MODIFIER.resetGame, false);
+document.getElementById(`pause-game`).addEventListener(`click`, MODIFIER.pauseGame, false);
+MODIFIER.optionsListener();
 
-// MODIFIER.startGame();
+//========================DOCUMENT READY============================================
+
 VIEW.generateCanvas();
-
-//-----------------------------------------------------------------------------------------------------------
-// const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
-
-// const resolution = 10;
-// canvas.width = 800;
-// canvas.height = 800;
-
-// const COLS = canvas.width / resolution;
-// const ROWS = canvas.height / resolution;
-
-// function buildGrid() {
-//     return Array(COLS).fill(null)
-//         .map(() => Array(ROWS).fill(null)
-//             .map(() => Math.floor(Math.random() * 2)));
-// }
-
-// let grid = buildGrid();
-
-// requestAnimationFrame(update);
-
-// function update() {
-//     grid = nextGen(grid);
-//     render(grid);
-//     requestAnimationFrame(update);
-// }
-// //-------------------------------------
-// function nextGen(grid) {
-//     nextGen = grid.map(arr => [...arr]);
-
-//     for (let col = 0; col < grid.length; col++) {
-//         for (let row = 0; row < grid[col].length; row++) {
-//             const cell = grid[col][row];
-//             let numNeighbours = 0;
-//             for (let i = -1; i < 2; i++) {
-//                 for (let j = -1; j < 2; j++) {
-//                     if (i === 0 && j === 0) {
-//                         continue;
-//                     }
-//                     const x_cell = col + i;
-//                     const y_cell = row + j;
-
-//                     if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
-//                         const currentNeighbour = grid[col + i][row + j];
-//                         numNeighbours += currentNeighbour;
-//                     }
-//                 }
-//             }
-
-//             // rules
-//             if (cell === 1 && numNeighbours < 2) {
-//                 nextGen[col][row] = 0;
-//             } else if (cell === 1 && numNeighbours > 3) {
-//                 nextGen[col][row] = 0;
-//             } else if (cell === 0 && numNeighbours === 3) {
-//                 nextGen[col][row] = 1;
-//             }
-//         }
-//     }
-//     return nextGen;
-// }
-
-// function render(grid) {
-//     for (let col = 0; col < grid.length; col++) {
-//         for (let row = 0; row < grid[col].length; row++) {
-//             const cell = grid[col][row];
-
-//             ctx.beginPath();
-//             ctx.rect(col * resolution, row * resolution, resolution, resolution);
-//             ctx.fillStyle = cell ? 'black' : 'white';
-//             ctx.fill();
-//             // ctx.stroke();
-//         }
-//     }
-// }
