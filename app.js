18a189db10b80a4c83d070b50bdbd0fb0dea1bdd
Mario Lema <lema_mario@yahoo.com>
2019-04-04 16:05:20 +0100

    comments and symplification

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const DATA = {
 //===========================================VIEW METHODS========================================================
 const VIEW = {
 
+
     generateCanvas() {
         DATA.canvas.width = DATA.width;
         DATA.canvas.height = DATA.height;
@@ -33,22 +34,25 @@ const VIEW = {
         this.render(DATA.baseArr);
     },
 
+    //DISABLES OPTIONS WHEN GAME IS ONGOING
     disableOptions(bool) {
         document.querySelectorAll(`input`).forEach((x) => bool ? x.disabled = true : x.disabled = false);
     },
 
+    //RENDERS THE ARRAY OF ARRAYS INTO THE CANVAS
     render(grid) {
         let ctx = DATA.canvas.getContext(`2d`);
-        ctx.clearRect(0, 0, DATA.width, DATA.height);
+        ctx.clearRect(0, 0, DATA.width, DATA.height); //clears the previous state of the canvas
+
         for (let col = 0; col < grid.length; col++) {
             for (let row = 0; row < grid[col].length; row++) {
 
 
-                if (DATA.canvasStyle === `emojimap`) {
+                if (DATA.canvasStyle === `emojimap`) { //if style is emoji, fill with text
                     let cell = MODIFIER.getCellText(grid[col][row]);
                     ctx.font = `${DATA.fontSize} Arial`;
                     ctx.fillText(cell, col * DATA.resolution, row * DATA.resolution);
-                } else {
+                } else { //else fill with rects
                     let cellColor = MODIFIER.getCellColor(grid[col][row]);
                     ctx.beginPath();
                     ctx.rect(col * DATA.resolution, row * DATA.resolution, DATA.resolution, DATA.resolution);
@@ -66,15 +70,19 @@ const VIEW = {
 //===========================================MODIFIER METHODS========================================================
 const MODIFIER = {
 
+    //================PLAY GAME METHODS==================
+    //===================================================
+
+    //STARTS THE GAME ON PLAY AND CALLS THE FIRST REQUESTANIMATIONFRAME
     startGame() {
         if (!DATA.playing) {
             DATA.playing = true;
             VIEW.disableOptions(true);
             requestAnimationFrame(this.update);
         }
-
     },
 
+    //RESETS THE GAME TO ORIGINAL STATE, MAINTAINING OPTIONS. STOPS TIMEOUT
     resetGame() {
         DATA.playing = false;
         VIEW.disableOptions(false);
@@ -82,6 +90,7 @@ const MODIFIER = {
         clearTimeout(DATA.timeout);
     },
 
+    //STOPS TIMEOUT AND LEAVES CANVAS AS IT IS
     pauseGame() {
         if (DATA.playing) {
             DATA.playing = false;
@@ -90,12 +99,26 @@ const MODIFIER = {
         }
     },
 
+    //SERVES AS RECURRENT FUNCTION TO CHANGE THE STATE OF THE CANVAS
+    update() {
+        this.nextGen();
+        VIEW.render(DATA.nextArr);
+        DATA.timeout = setTimeout(() => {
+            requestAnimationFrame(this.update);
+        }, DATA.intervalTime);
+    },
+
+
+    //================UTILITY METHODS==================
+    //=================================================
+
+    //CREATES AN ARRAY OF ARRAYS
     buildGrid() {
         return Array(DATA.cols()).fill(null)
             .map(() => Array(DATA.rows()).fill(null)
                 .map(() => this.populationAlg()));
     },
-
+    //POPULATES THE ORIGINAL ARRAY WITH MORE OR LESS LIVE CELLS DEPENDING ON SELECTION
     populationAlg() {
         let binary;
         let random = Math.random() * 2;
@@ -109,101 +132,55 @@ const MODIFIER = {
         return binary;
     },
 
-
-    update() {
-        this.nextGen();
-        VIEW.render(DATA.nextArr);
-        DATA.timeout = setTimeout(() => {
-            requestAnimationFrame(this.update);
-        }, DATA.intervalTime);
-
-    },
-
-
-
+    //RETURNS A DIFFERENT COLOR DEPENDING ON THE VALUE OF THE CELL PASSED
     getCellColor(cell) {
-        let color;
-        if (DATA.canvasStyle === `black-white`) {
-            color = cell > 0 ? `#333333` : `#E6E6E6`;
-        } else {
-            if (cell <= -3) {
-                color = `#142ead`;
-            } else if (cell === -2) {
-                color = `#4761E0`;
-            } else if (cell === -1) {
-                color = `#7A94FF`;
-            } else if (cell === 0) {
-                color = `#ADC7FF`;
-            } else if (cell === 1) {
-                color = `#D1FFAD`;
-            } else if (cell === 2) {
-                color = `#9EFF7A`;
-            } else if (cell === 3) {
-                color = `#6BE047`;
-            } else {
-                color = `#38ad14`;
-            }
-        }
-        return color;
+        return cell <= -3 ? `#142ead` : cell === -2 ? `#4761E0` : cell === -1 ? `#7A94FF` : cell === 0 ? `#ADC7FF` : cell === 1 ? `#D1FFAD` : cell === 2 ? `#9EFF7A` : cell === 3 ? `#6BE047` : `#38ad14`;
     },
 
+    //RETURNS A DIFFERENT EMOJI DEPENDING ON THE VALUE OF THE CELL PASSED
     getCellText(cell) {
-        let emoji;
-        if (cell <= -3) {
-            emoji = `🧟`;
-        } else if (cell === -2) {
-            emoji = `👻`;
-        } else if (cell === -1) {
-            emoji = `⚰️`;
-        } else if (cell === 0) {
-            emoji = `💀`;
-        } else if (cell === 1) {
-            emoji = `👶`;
-        } else if (cell === 2) {
-            emoji = `🧑`;
-        } else if (cell === 3) {
-            emoji = `🧓`;
-        } else {
-            emoji = `🧙`;
-        }
-        return emoji;
+        return cell <= -3 ? `🧟` : cell === -2 ? `👻` : cell === -1 ? `⚰️` : cell === 0 ? `💀` : cell === 1 ? `👶` : cell === 2 ? `🧑` : cell === 3 ? `🧓` : `🧙`;
     },
 
+    //GENERATES A NEW ARRAY BASED ON THE PREVIOUS ONE AND SETS THE ORIGINAL TO THIS ONE
     nextGen() {
         DATA.nextArr = DATA.baseArr.map(arr => [...arr]);
 
         for (let col = 0; col < DATA.baseArr.length; col++) {
             for (let row = 0; row < DATA.baseArr[col].length; row++) {
 
-                let cell = this.cellNextState(col, row);
-                DATA.nextArr[col][row] = cell;
+                DATA.nextArr[col][row] = this.cellNextState(col, row);
             }
         }
 
         DATA.baseArr = DATA.nextArr.map(arr => [...arr]);
-
     },
 
+    //CALCULATES THE VALUE OF THE CURRENT CELL DEPENDING ON ITS CURRENT NEIGHBOURS
     cellNextState(col, row) {
-        let grid = DATA.baseArr;
-        let cell = grid[col][row];
-        let lastIndex = grid.length - 1;
-        let prevRow = row - 1;
-        let prevCol = col - 1;
-        let nextRow = row + 1;
-        let nextCol = col + 1;
-        let liveNeigh = 0;
-        
+        //VARIABLES FOR UNDERSTANDABLE MANIPULATION
+        let grid = DATA.baseArr,
+            cell = grid[col][row],
+            lastIndex = grid.length - 1,
+            prevRow = row - 1,
+            prevCol = col - 1,
+            nextRow = row + 1,
+            nextCol = col + 1,
+            liveNeigh = 0,
 
-        let topCenter = grid[col][prevRow] ? grid[col][prevRow] : grid[col][lastIndex];
-        let leftCenter = grid[prevCol] ? grid[prevCol][row] : grid[lastIndex][row];
-        let rightCenter = grid[nextCol] ? grid[nextCol][row] : grid[0][row];
-        let bottomCenter = grid[col][nextRow] ? grid[col][nextRow] : grid[col][0];
-        let topRight;
-        let bottomLeft;
-        let bottomRight;
-        let topLeft;
+            //CROSS SECTION NEIGHBOURS
+            topCenter = grid[col][prevRow] ? grid[col][prevRow] : grid[col][lastIndex],
+            leftCenter = grid[prevCol] ? grid[prevCol][row] : grid[lastIndex][row],
+            rightCenter = grid[nextCol] ? grid[nextCol][row] : grid[0][row],
+            bottomCenter = grid[col][nextRow] ? grid[col][nextRow] : grid[col][0],
 
+            //DIAGONAL SECTION NEIGHBOURS
+            topRight,
+            bottomLeft,
+            bottomRight,
+            topLeft;
+
+        //right side of the grid
         if (grid[nextCol]) {
             //topright
             if (grid[nextCol][prevRow]) {
@@ -234,7 +211,7 @@ const MODIFIER = {
             }
         }
 
-
+        //left side of the grid
         if (grid[prevCol]) {
             //bottom left
             if (grid[prevCol][nextRow]) {
@@ -265,12 +242,16 @@ const MODIFIER = {
         }
 
 
-        let liveNeighArr = [topCenter, leftCenter, rightCenter, bottomCenter, topRight, bottomLeft, bottomRight, topLeft];
+        //iterate over neighbours and add 1 to live neighbours if it it is alive;
+        [topCenter, leftCenter, rightCenter, bottomCenter, topRight, bottomLeft, bottomRight, topLeft].map(x => {
+            if (x > 0) liveNeigh++
+        });
 
-        for (let i = 0; i < liveNeighArr.length; i++) {
-            if (liveNeighArr[i] > 0) liveNeigh++;
-        }
+        return this.ruling(cell, liveNeigh);
+    },
 
+    //RULES IF A CELL SHOULD LIVE OR DIE DEPENDING ON ITS NEIGHBOURS
+    ruling(cell, liveNeigh) {
         if (cell <= 0) {
             if (liveNeigh === 3) {
                 cell = 1;
@@ -287,88 +268,75 @@ const MODIFIER = {
         return cell;
     },
 
+    //HANDLER FOR ALL OF THE OPTIONS
     optionsChanger() {
-        if (!DATA.playing) {
-            let id = event.target.id;
-            switch (id) {
-                case `slow`:
-                    DATA.intervalTime = 1000;
-                    break;
-                case `regular`:
-                    DATA.intervalTime = 500;
-                    break;
-                case `fast`:
-                    DATA.intervalTime = 0;
-                    break;
-                case `black-white`:
-                    DATA.canvasStyle = `black-white`;
-                    VIEW.generateCanvas();
-                    break;
-                case `heatmap`:
-                    DATA.canvasStyle = `heatmap`;
-                    VIEW.generateCanvas();
-                    break;
-                case `emojimap`:
+        let id = event.target.id;
+        switch (id) {
+            case `slow`:
+                DATA.intervalTime = 1000;
+                break;
+            case `regular`:
+                DATA.intervalTime = 500;
+                break;
+            case `fast`:
+                DATA.intervalTime = 0;
+                break;
+            case `black-white`:
+                DATA.canvasStyle = `black-white`;
+                VIEW.generateCanvas();
+                break;
+            case `heatmap`:
+                DATA.canvasStyle = `heatmap`;
+                VIEW.generateCanvas();
+                break;
+            case `emojimap`:
+                DATA.resolution = 50;
+                DATA.fontSize = `2.5rem`;
+                DATA.canvasStyle = `emojimap`;
+                VIEW.generateCanvas();
+                break;
+            case `small`:
+                DATA.width = 100;
+                DATA.height = 100;
+                DATA.resolution = 10;
+                DATA.fontSize = `0.5`;
+                VIEW.generateCanvas();
+                break;
+            case `medium`:
+                DATA.width = 500;
+                DATA.height = 500;
+                DATA.resolution = 20;
+                DATA.fontSize = `1rem`;
+                VIEW.generateCanvas();
+                break;
+            case `large`:
+                DATA.width = 1000;
+                DATA.height = 1000;
+                if (DATA.canvasStyle === `emojimap`) {
                     DATA.resolution = 50;
                     DATA.fontSize = `2.5rem`;
-                    DATA.canvasStyle = `emojimap`;
-                    VIEW.generateCanvas();
-                    break;
-                case `small`:
-                    DATA.width = 100;
-                    DATA.height = 100;
+                } else {
                     DATA.resolution = 10;
-                    DATA.fontSize = `0.5`;
-                    VIEW.generateCanvas();
-                    break;
-                case `medium`:
-                    DATA.width = 500;
-                    DATA.height = 500;
-                    DATA.resolution = 20;
-                    DATA.fontSize = `1rem`;
-                    VIEW.generateCanvas();
-                    break;
-                case `large`:
-                    DATA.width = 1000;
-                    DATA.height = 1000;
-                    if (DATA.canvasStyle = `emojimap`) {
-                        DATA.resolution = 50;
-                        DATA.fontSize = `2.5rem`;
-                    } else {
-                        DATA.resolution = 10;
-                    }
-                    VIEW.generateCanvas();
-                    break;
-                case `low`:
-                    DATA.population = `low`;
-                    VIEW.generateCanvas();
-                    break;
-                case `average`:
-                    DATA.population = `average`;
-                    VIEW.generateCanvas();
-                    break;
-                case `high`:
-                    DATA.population = `high`;
-                    VIEW.generateCanvas();
-                    break;
-                default:
-                    return
-            }
-
+                }
+                VIEW.generateCanvas();
+                break;
+            case `low`:
+                DATA.population = `low`;
+                VIEW.generateCanvas();
+                break;
+            case `average`:
+                DATA.population = `average`;
+                VIEW.generateCanvas();
+                break;
+            case `high`:
+                DATA.population = `high`;
+                VIEW.generateCanvas();
+                break;
+            default:
+                return
         }
+
     },
-    //================PLAY GAME METHODS==================
-    //===================================================
-
-
-    //================VALIDATION METHODS==================
-    //===================================================
-
-
-    //================UTILITY METHODS==================
-    //===================================================
-
-
 
     //================LISTENER METHODS==================
     //===================================================
@@ -390,73 +358,4 @@ document.getElementById(`pause-game`).addEventListener(`click`, MODIFIER.pauseGa
 MODIFIER.optionsListener();
 
 //========================DOCUMENT READY============================================
-
 VIEW.generateCanvas();
-
-
-// let topCenter = DATA.baseArr[col][row - 1] !== undefined ? DATA.baseArr[col][row - 1] : DATA.baseArr[col][lastIndex];
-// let leftCenter = DATA.baseArr[col - 1][row] !== undefined ? DATA.baseArr[col - 1][row] : DATA.baseArr[lastIndex][row];
-// let rightCenter = DATA.baseArr[col + 1][row] !== undefined ? DATA.baseArr[col + 1][row] : DATA.baseArr[0][row];
-// let bottomCenter = DATA.baseArr[col][row + 1] !== undefined ? DATA.baseArr[col][row + 1] : DATA.baseArr[col][0];
-
-
-// let topRight = DATA.baseArr[nextCol][prevRow] !== undefined ?
-// DATA.baseArr[nextCol][prevRow] : 
-// DATA.baseArr[0][lastIndex] !== undefined ?
-// DATA.baseArr[0][lastIndex] :
-// DATA.baseArr[nextCol][lastIndex] !== undefined ?
-// DATA.baseArr[nextCol][lastIndex] :
-// DATA.baseArr[0][prevRow];
-
-
-// let bottomLeft = DATA.baseArr[prevCol][nextRow] !== undefined ? 
-// DATA.baseArr[prevCol][nextRow] : 
-// DATA.baseArr[lastIndex][0] !== undefined ?
-// DATA.baseArr[lastIndex][0] :
-// DATA.baseArr[prevCol][0] !== undefined ?
-// DATA.baseArr[prevCol][0] :
-// DATA.baseArr[lastIndex][nextRow];
-
-// let bottomRight = DATA.baseArr[nextCol][nextRow] !== undefined ? 
-//    DATA.baseArr[nextCol][nextRow] :
-//    DATA.baseArr[0][0] !== undefined ?
-//    DATA.baseArr[0][0] :
-//    DATA.baseArr[nextCol][0] !== undefined ?
-//    DATA.baseArr[nextCol][0] :
-//    DATA.baseArr[0][nextRow];
-
-// let topLeft = DATA.baseArr[prevCol][prevRow] !== undefined ?
-//    DATA.baseArr[prevCol][prevRow] :
-//    DATA.baseArr[lastIndex][lastIndex] !== undefined ?
-//    DATA.baseArr[lastIndex][lastIndex] :
-//    DATA.baseArr[prevCol][lastIndex] !== undefined ?
-//    DATA.baseArr[prevCol][lastIndex] :
-//    DATA.baseArr[lastIndex][prevRow];
-
-
-
-// //topcenter
-// if(grid[col][prevRow]){
-//     topCenter = grid[col][prevRow];
-// }else{
-//     topCenter = grid[col][lastIndex];
-// }
-// //leftcenter
-// if(grid[prevCol]){
-//     leftCenter =  grid[prevCol][row];
-// }else{
-//     leftCenter = grid[lastIndex][row];
-// }
-// //rightcenter
-// if(grid[nextCol]){
-//     rightCenter = grid[nextCol][row];
-// }else{
-//     rightCenter = grid[0][row];
-// }
-// //bottomcenter
-// if(grid[col][nextRow]){
-//     bottomCenter = grid[col][nextRow];
-
-// }else{
-//     bottomCenter = grid[col][0];
-// }
